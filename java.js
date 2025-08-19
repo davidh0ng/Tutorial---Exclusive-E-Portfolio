b6c4a57f408dc7e08d8cd6bb65fcf567336fd8d2
davidh0ng <djhong83@gmail.com>
2025-08-19 01:42:02 -0700

    added js code to modals to accept input and send to emailjs

--- java.js
+++ java.js
@@ -1,1 +1,27 @@
-console.log("hello")
+// service_ozw9njd
+// template_hjulza7
+// fiG2VO-usTCySp2QC
+
+function contact(event) {
+  event.preventDefault(event);
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_ozw9njd",
+      "template_hjulza7",
+      event.target,
+      "fiG2VO-usTCySp2QC"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on djhong83@gmail.com."
+      );
+    });
+}
